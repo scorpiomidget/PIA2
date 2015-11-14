872150f11eb2eabe7eb372e396cd3a554f3d4762
scorpiomidget <scorpiomidget@gmail.com>
2015-11-14 10:27:34 +0000

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,17 +1,32 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
+// Setup scraping parameters
+$keywords = "4894128028666";
+
+$url = "https://www.google.co.uk/search?q=".$keywords;
+
+$params = array(
+  );
+  
+$userAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+
+// Read in a page
+$html = scraperwiki::scrape($url, $params, $userAgent);
+
+// Find something on the page using css selectors
+$dom = new simple_html_dom();
+$dom->load($html);
+//print_r($dom->find(".r a"));
+$items = $dom->find(".r a"));
+
+foreach($items AS $i=>$item) {
+   echo $i.': '.$item->attr['href'].'<br />';
+}
+
 //
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
